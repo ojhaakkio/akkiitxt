61301b351eafca03e6c1595e11e858d8b8c5fcf7
ojhaakkio <157199909+ojhaakkio@users.noreply.github.com>
2024-02-08 21:42:01 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -7,7 +7,7 @@ import os
 import sys
 
 
-@stark.on_message(filters.command(["start"]) & ~filters.edited)
+@stark.on_message(filters.command(["start"]) & ~filters.private)
 async def Start_msg(bot: stark , m: Message):
     await bot.send_photo(
     m.chat.id,
@@ -18,11 +18,11 @@ async def Start_msg(bot: stark , m: Message):
            
 
 
-@stark.on_message(filters.command(["restart"]) & ~filters.edited)
+@stark.on_message(filters.command(["Restarted"]) & ~filters.private)
 async def restart_handler(_, m):
     await m.reply_text("Restarted!", True)
     os.execl(sys.executable, sys.executable, *sys.argv)
 
-@stark.on_message(filters.command(["log"]) & ~filters.edited)
+@stark.on_message(filters.command(["log"]) & ~filters.private)
 async def log_msg(bot: stark , m: Message):   
     await bot.send_document(m.chat.id, "log.txt")
